e0924c38047bdc2d02b1541e8552ec98eb7a878a
joo-chang <jooc0311@gmail.com>
2024-08-23 11:20:06 +0900

    init commit

---
+++ src/main/java/com/sparta/balloondelivery/BalloonDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.balloondelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalloonDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalloonDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/balloondelivery/BalloonDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.balloondelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalloonDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
