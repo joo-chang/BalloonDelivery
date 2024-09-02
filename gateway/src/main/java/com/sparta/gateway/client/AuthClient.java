6d01facf857031114e3a28c74d42f1c7e8ba790d
Kim JinSeon <kwlstjs00@gmail.com>
2024-09-02 16:19:29 +0900

    delete: feign client url

--- gateway/src/main/java/com/sparta/gateway/client/AuthClient.java
+++ gateway/src/main/java/com/sparta/gateway/client/AuthClient.java
@@ -3,8 +3,8 @@ package com.sparta.gateway.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+@FeignClient(name = "BalloonDelivery")
 
-@FeignClient(name = "BalloonDelivery", url = "http://localhost:19093")
 public interface AuthClient {
     @GetMapping("/auth/getPermission/{userId}")
     String getPermission(@PathVariable Long userId);
